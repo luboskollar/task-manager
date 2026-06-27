2a81f54d66f983f9c1442dc4b334e7a77aac09be
luboskollar <lubos.kollar444@gmail.com>
2026-06-27 11:24:10 +0200

    Add TodoRepository with basic CRUD

---
+++ TaskManager/Repositories/TodoRepository.cs
@@ -0,0 +1,24 @@
+﻿using TaskManager.Models;
+
+namespace TaskManager.Repositories;
+
+public class TodoRepository
+{
+    private List<TodoItem> _todoItems = new();
+    private int _nextId = 1;
+    
+    public void Add(TodoItem todoItem)
+    {
+        _todoItems.Add(todoItem);
+    }
+
+    public IReadOnlyList<TodoItem> GetAll()
+    {
+        return _todoItems;
+    }
+    
+    public TodoItem? GetById(int id)
+    {
+        return _todoItems.FirstOrDefault(t => t.Id == id);
+    }
+}
