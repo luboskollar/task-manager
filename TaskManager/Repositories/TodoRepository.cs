4a8fe325f5a272cb2bb45c84080d76aae6d9d935
luboskollar <lubos.kollar444@gmail.com>
2026-06-27 11:32:59 +0200

    Add Delete and Update to TodoRepository

--- TaskManager/Repositories/TodoRepository.cs
+++ TaskManager/Repositories/TodoRepository.cs
@@ -11,7 +11,28 @@ public class TodoRepository
     {
         _todoItems.Add(todoItem);
     }
+    public bool Delete(int id)
+    {
+        var todo = GetById(id);
+        if (todo != null)
+        {
+            _todoItems.Remove(todo);
+            return true;
+        }
+        return false;
+    }
 
+    public bool Update(TodoItem updatedTodoItem)
+    {
+        int index = _todoItems.FindIndex(t => t.Id == updatedTodoItem.Id);
+        if (index >= 0)
+        {
+            _todoItems[index] = updatedTodoItem;
+            return true;
+        }
+        return false;
+    }
+    
     public IReadOnlyList<TodoItem> GetAll()
     {
         return _todoItems;
