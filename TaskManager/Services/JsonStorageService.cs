7edf4c33c85edc4157668b96280ff28f26eee1f8
luboskollar <lubos.kollar444@gmail.com>
2026-06-28 11:43:10 +0200

    Add JsonStorageService

---
+++ TaskManager/Services/JsonStorageService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using TaskManager.Models;
+
+namespace TaskManager.Services;
+
+public class JsonStorageService
+{
+    private const string FileName = "todos.json";
+    
+    public void Save(List<TodoItem> todoItems)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        File.WriteAllText(FileName, JsonSerializer.Serialize(todoItems, options));
+    }
+
+    public List<TodoItem> Load()
+    {
+        if (File.Exists(FileName))
+        {
+            return JsonSerializer.Deserialize<List<TodoItem>>(File.ReadAllText(FileName)) ?? new List<TodoItem>();
+        }
+        return new List<TodoItem>();
+    }
+}
